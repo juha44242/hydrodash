712821b25e56ab44c6c1ea6b345764a83dc1dead
juha44242 <104754622+juha44242@users.noreply.github.com>
2023-12-12 08:59:53 +0100

    Update chart.js

--- js/chart.js
+++ js/chart.js
@@ -1,8 +1,7 @@
-<script>
 const xValues = [50,60,70,80,90,100,110,120,130,140,150];
 const yValues = [7,8,8,9,9,9,10,11,14,14,15];
 
-new Chart("myChart", {
+new Chart("line"), {
   type: "line",
   data: {
     labels: xValues,
@@ -20,5 +19,4 @@ new Chart("myChart", {
       yAxes: [{ticks: {min: 6, max:16}}],
     }
   }
-});
-</script>
+};
