7c46a16d8576b567588652982552311412ee020d
ling53933 <132983445+ling53933@users.noreply.github.com>
2023-12-12 06:40:11 +0100

    created gallery

--- js/plantebib.js
+++ js/plantebib.js
@@ -1,35 +1,15 @@
-// Gallery data
-var galleryData = [
-    { image: "img_5terre.jpg", desc: "Cinque Terre" },
-    { image: "img_forest.jpg", desc: "Forest" },
-    { image: "img_lights.jpg", desc: "Northern Lights" },
-    { image: "img_mountains.jpg", desc: "Mountains" }
-  ];
+function openModal(text) {
+    document.getElementById('modalText').innerText = text;
+    document.getElementById('myModal').style.display = 'flex';
+}
 
-  // Create popups for each gallery item
-  for (var i = 0; i < galleryData.length; i++) {
-    var galleryId = "gallery" + (i + 1);
-    var galleryItem = document.getElementById(galleryId);
+function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+}
 
-    galleryItem.onclick = function(index) {
-      return function() {
-        openPopup(index);
-      };
-    }(i);
-  }
-
-  // Function to open the popup
-  function openPopup(index) {
-    var popup = document.getElementById("popup");
-    var popupImage = document.getElementById("popupImage");
-    var popupDesc = document.getElementById("popupDesc");
-
-    popupImage.src = galleryData[index].image;
-    popupDesc.innerHTML = galleryData[index].desc;
-    popup.style.display = "block";
-  }
-
-  // Function to close the popup
-  function closePopup() {
-    document.getElementById("popup").style.display = "none";
-  }
+// Close the modal if the user clicks outside of it
+window.onclick = function (event) {
+    if (event.target === document.getElementById('myModal')) {
+        closeModal();
+    }
+};
