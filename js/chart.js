0788825c0ea4416f4bdc350cd73e2a502b03c3be
juha44242 <104754622+juha44242@users.noreply.github.com>
2023-12-12 09:11:10 +0100

    Update chart.js

--- js/chart.js
+++ js/chart.js
@@ -20,3 +20,77 @@ new Chart("line"), {
     }
   }
 };
+
+
+// line chart //
+
+var lastTime = new Date('12 Dec 2023 08:25:00 GMT').getTime();
+var data = [];
+
+function getData() {
+    data.shift();
+
+    while (data.length < 20) {
+        data.push({
+            time: new Date((lastTime += 1000)),
+            voltage: 1.1 + Math.random() / 2,
+        });
+    }
+
+    return data;
+}
+
+const options = {
+    container: document.getElementById('myChart'),
+    data: getData(),
+    series: [
+        {
+            xKey: 'time',
+            yKey: 'voltage',
+        },
+    ],
+    axes: [
+        {
+            type: 'time',
+            position: 'bottom',
+            nice: false,
+            tick: {
+                interval: agCharts.time.second.every(5),
+            },
+            label: {
+                format: '%H:%M:%S',
+            },
+        },
+        {
+            type: 'number',
+            position: 'left',
+            label: {
+                format: '#{.2f}V',
+            },
+        },
+    ],
+    title: {
+        text: 'Core Voltage',
+    },
+};
+
+const chart = agCharts.AgCharts.create(options);
+var updating = false;
+
+function startUpdates() {
+    if (updating) {
+        return;
+    }
+
+    updating = true;
+    //@ts-ignore
+    this.update();
+    //@ts-ignore
+    setInterval(this.update, 500);
+}
+
+/** inScope */
+function update() {
+    options.data = getData();
+    agCharts.AgCharts.update(chart, options);
+}
