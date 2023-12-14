7a3557dfa3be6fe2951007e5b38efc3a9fb51ed3
ling53933 <132983445+ling53933@users.noreply.github.com>
2023-12-14 09:00:27 +0100

    sheet bar added to dashbord

--- js/index.js
+++ js/index.js
@@ -27,4 +27,40 @@ function closeNav() {
     document.getElementById("hallo").innerHTML = greeting + " Camilla Andersen";
 
 
- 
+
+
+ //Sheet
+    let sheetCount = 1;
+
+    function addSheet() {
+      const sheetBar = document.getElementById('sheetBar');
+      const newSheet = document.createElement('div');
+      newSheet.classList.add('sheet');
+      newSheet.textContent = `Sheet ${sheetCount}`;
+      newSheet.onclick = function() {
+        switchSheet(this.textContent);
+      };
+  
+      sheetBar.insertBefore(newSheet, sheetBar.lastChild);
+  
+      // Make the first sheet active by default
+      if (sheetCount === 1) {
+        switchSheet('Sheet 1');
+      }
+  
+      sheetCount++;
+    }
+  
+    function switchSheet(sheetName) {
+      // Hide all content pages
+      const contentPages = document.querySelectorAll('.content-page');
+      contentPages.forEach(page => page.classList.remove('active-page'));
+  
+      // Create a new content page for the selected sheet
+      const newContentPage = document.createElement('div');
+      newContentPage.classList.add('content-page', 'active-page');
+      newContentPage.textContent = `This is the content for ${sheetName}`;
+      
+      // Append the new content page
+      document.getElementById('contentPages').appendChild(newContentPage);
+    }
